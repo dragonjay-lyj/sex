efcdbaf58a03f593c590a36ff1d832dd1d288a84
dragonjay-lyj <liujinlisb@gmail.com>
2025-04-19 22:24:54 +0800

    modified:   astro.config.mjs 	deleted:    package-lock.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,16 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
+  site: 'https://edge.dragonjay.top',
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
